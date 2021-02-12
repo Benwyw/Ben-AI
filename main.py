e91411e2675d19e7ae5a45e6693d649f5ecebe83
Ben <57297093+Benwyw@users.noreply.github.com>
2021-02-13 03:20:42 +0800

    Debug

--- main.py
+++ main.py
@@ -40,8 +40,8 @@ class YTDLError(Exception):
 class YTDLSource(discord.PCMVolumeTransformer):
     YTDL_OPTIONS = {
         'format': 'bestaudio/best',
-        #'extractaudio': True,
-        #'audioformat': 'mp3',
+        'extractaudio': True,
+        'audioformat': 'mp3',
         'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
         'restrictfilenames': True,
         'noplaylist': True,
