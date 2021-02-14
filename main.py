1e89c27b1eabaae14636454cbb4025bc88cb3b5a
Ben <57297093+Benwyw@users.noreply.github.com>
2021-02-14 15:23:37 +0800

    Shield updates

--- main.py
+++ main.py
@@ -597,7 +597,10 @@ async def on_message(message):
     #Shield
     if 'ben' in message.content.lower() and 'gay' in message.content.lower():
         await message.delete()
-        await message.channel.send(str(message.author.display_name)+" is gay")
+        if 'ben' in message.author.display_name:
+            message.channel.send("Pok is gay")
+        else:
+            await message.channel.send(str(message.author.display_name)+" is gay")
 
 load_dotenv()
 bot.run(os.getenv('TOKEN'))
