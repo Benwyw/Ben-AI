9ace85f74238d407f664c97ec53ca9f185d1580b
Ben <57297093+Benwyw@users.noreply.github.com>
2021-06-10 19:16:24 +0800

    Updated DB web

--- web.py
+++ web.py
@@ -1,12 +1,45 @@
-from sanic import Sanic
-from sanic.response import json
 import os
+import main
+
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from DBConnection import DBConnection
+
+from sanic import Sanic
+from sanic.response import text
+
+global data
+data = 'null'
+
+async def fetch():
+    global data
+    data = ''
+    rankData = DBConnection.fetchAllRankData()
+
+    count = 1
+    for user in rankData:
+        tempID = user[0]
+        tempWIN = user[1]
+
+        if count <= 5:
+            user = await main.bot.fetch_user(tempID)
+
+            data += "{}. {} (勝場: {})\n".format(count, user.display_name, tempWIN)
+
+        count += 1
 
 app = Sanic("app")
 
+@app.listener('before_server_start')
+async def initialize_scheduler(app, loop):
+    await fetch()
+    scheduler = AsyncIOScheduler({'event_loop': loop})
+    scheduler.add_job(fetch, 'interval', hours=1)
+    scheduler.start()
+
 @app.route('/')
 async def test(request):
-    return json({'hello': 'world'})
+    global data
+    return text('{}\nUpdated every hour.'.format(data))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=os.getenv('PORT'))
+    app.run(host='0.0.0.0', port=os.getenv('PORT'), debug=False, access_log=False)
