5d6e31e830022a3debaaf81f9b8bbea0b291652b
Ben <57297093+Benwyw@users.noreply.github.com>
2021-06-10 20:03:14 +0800

    Added date time for updated timestamps

--- web.py
+++ web.py
@@ -1,16 +1,18 @@
-import os
+import os, pytz
 
+from datetime import datetime
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from DBConnection import DBConnection
 
 from sanic import Sanic
 from sanic.response import text
 
-global data
+global data, hk_now
 data = 'null'
+hk_now = 'null'
 
 async def fetch():
-    global data
+    global data, hk_now
     data = ''
     rankData = DBConnection.fetchAllRankData()
 
@@ -19,7 +21,7 @@ async def fetch():
         tempID = str(user[0])
         tempWIN = str(user[1])
 
-        if count <= 5:
+        if count <= 10:
             tempStar = ''
             for i in range(1,len(tempID)):
                 tempStar += '*'
@@ -29,6 +31,9 @@ async def fetch():
             data += "{}. {} (勝場: {})\n".format(count, userID, tempWIN)
 
         count += 1
+
+    tz = pytz.timezone('Asia/Hong_Kong')
+    hk_now = datetime.now(tz)
 
 app = Sanic("app")
 
@@ -41,8 +46,8 @@ async def initialize_scheduler(app, loop):
 
 @app.route('/')
 async def test(request):
-    global data
-    return text('{}\nUpdated every hour.'.format(data))
+    global data, hk_now
+    return text('{}\nUpdated: {}'.format(data, hk_now))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=os.getenv('PORT'), debug=False, access_log=False)
