c2e7d8a3da263dbcf93848accd90bd6cb49e2775
Ben <57297093+Benwyw@users.noreply.github.com>
2022-01-01 15:12:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -795,8 +795,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
         'no_warnings': True,
         'default_search': 'auto',
         'source_address': '0.0.0.0',
-        'verbose': True,
-        'update': True
+        'verbose': True
     }
 
     FFMPEG_OPTIONS = {
