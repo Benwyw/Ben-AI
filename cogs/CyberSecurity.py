c83c27299f3e2b4c4da1dae7444308f48dcbc97f
Ben <57297093+Benwyw@users.noreply.github.com>
2022-05-26 15:02:47 +0800

    cooldown from 4-60 to 2-60

--- cogs/CyberSecurity.py
+++ cogs/CyberSecurity.py
@@ -4,7 +4,7 @@ class CyberSecurity(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         
-    @slash_command(guild_ids=guild_ids, name='scan', description='Retrieve virus scan report of the url', cooldown=commands.CooldownMapping.from_cooldown(4, 60, commands.BucketType.default), description_localizations={"zh-TW": "提取網址的病毒掃描報告"})
+    @slash_command(guild_ids=guild_ids, name='scan', description='Retrieve virus scan report of the url', cooldown=commands.CooldownMapping.from_cooldown(2, 60, commands.BucketType.default), description_localizations={"zh-TW": "提取網址的病毒掃描報告"})
     #@commands.cooldown(1, 600, commands.BucketType.user)
     async def _scan(self, ctx:commands.Context, url:Option(str, "Paste the url", name_localizations={"zh-TW": "貼上網址"})):
         await ctx.defer()
