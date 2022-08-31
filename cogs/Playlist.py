47accc61d2fd30fdfd7879e5d55d5ba4db611768
Ben <57297093+Benwyw@users.noreply.github.com>
2022-08-31 14:07:10 +0800

    Update Playlist.py

--- cogs/Playlist.py
+++ cogs/Playlist.py
@@ -8,10 +8,12 @@ class Playlist(commands.Cog):
     @slash_command(guild_ids=guild_ids, name='testplaylist', description='Testing playlist', description_localizations={"zh-TW": "測試播放清單"})
     #@commands.cooldown(1, 600, commands.BucketType.user)
     #cooldown=commands.CooldownMapping.from_cooldown(2, 60, commands.BucketType.default),
-    async def _testplaylist(self, ctx:commands.Context, url:Option(str, "Test param", name_localizations={"zh-TW": "測試參數"})):
+    async def _testplaylist(self, ctx:commands.Context, desc:str):
+        #url:Option(str, "Test param", name_localizations={"zh-TW": "測試參數"})
         await ctx.defer()
         timestamp = str(datetime.now(pytz.timezone('Asia/Hong_Kong')))
-        await ctx.send_followup(ltp.Playlist.create_embed(url))
+        print('before send_followup')
+        await ctx.send_followup(ltp.Playlist.create_embed(desc))
 
 def setup(
     bot: commands.Bot
