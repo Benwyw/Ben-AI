2e3dcccae18594544ac6a64174700737ff40e2ac
Ben <57297093+Benwyw@users.noreply.github.com>
2022-09-06 17:07:40 +0800

    refactor with stock cmd bug fix

--- lib/GlobalVariables.py
+++ lib/GlobalVariables.py
@@ -15,3 +15,16 @@ channel_BrianLee_Satellite = 815568098001813555
 channel_BenKaChu_OffTopic = 772038210057535488
 
 Economy_imgUrl = "https://i.imgur.com/ydS4u8P.png"
+
+dmList = [254517813417476097,525298794653548751,562972196880777226,199877205071888384,407481608560574464,346518519015407626,349924747686969344,270781455678832641,363347146080256001,272977239014899713,262267347379683329,394354007650336769,372395366986940416,269394999890673664]
+
+music_command_List = ['/join','/leave','/loop','/now','/pause','/play','/queue','/remove','/resume','/shuffle','/skip','/stop','/summon','/volume',
+                          '/j','/disconnect','/v','/current','/playing','/r','/st','/s','/q','/rm','/l','/p',
+                          '/stock','/cov','/covid','/cov19','/covid-19']
+casino_command_List = ['/call','/fold','/highest','/pot','/raise',
+                        '/bal','/pay','/setbal',
+                        '/game','/hand','/in','/out','/rc','/setcolor','/setsort','/start',
+                        '/cards','/next',
+                        '/ctb','/enter','/pass',
+                        '/reward','/bonus','/b','/prize','/rank','/draw']
+minecraft_command_List = ['/bind', '/bound', '/unbind', '/ver', '/discver' '/dm', '/block', '/unblock', '/blocklist']
